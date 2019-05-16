c63138903239ae4dc102d4485fd5e3795ba8be34
Jondn Tryniski <jtrynisk@oswego.edu>
2019-05-16 11:03:42 -0400

    added a servermain method

---
+++ src/main/java/backend/ServerMain.java
@@ -0,0 +1,15 @@
+package backend;
+
+public class ServerMain {
+
+    public static void main(String[] args){
+
+        try {
+            Server s = new Server();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
